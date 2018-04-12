bd6bd31cc9407e3615319e649122b748b526b427
Kukikoe <ja.bondareva2014@yandex.ru>
2018-04-12 16:48:28 +0300

    first commit

---
+++ Two/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value: ");
+            string number = Console.ReadLine();
+            int two = 2;
+            int count = 0;
+            do
+            {
+                if (two * 2 > int.Parse(number) / 2 && two < int.Parse(number) / 2)
+                {
+                    two += 1;
+                    count++;
+                }
+                // if(int.Parse(number) % (two + 1) == 0)
+                else if ((two + 1) == int.Parse(number) / 2 )
+                {
+                    two += 1;
+                    count++;
+                }
+                //else if (int.Parse(number) / 2 - two <= 2 && int.Parse(number) / 2 - two > 0)
+                //{
+                //    two += 1;
+                //    count++;
+                //}
+                else if (int.Parse(number) - two == 1)
+                {
+                    two += 1;
+                    count++;
+                }
+                else
+                {
+                    two *= 2;
+                    count++;
+                }
+            } while (two != int.Parse(number));
+            Console.WriteLine("Count: " + count);
+
+            Console.ReadKey();
+        }
+    }
+}
